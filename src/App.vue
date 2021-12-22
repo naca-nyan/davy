fe8369dd9d5123ea1c333ef2ff9cb628d0efc42c
なかにゃん <73695982+naca-nyan@users.noreply.github.com>
2021-12-22 22:43:18 +0900

    Implement GET

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,68 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+  <input v-model="entrypoint" @change="fetchItems()" />
+  <h1>HEADER</h1>
+  <ul>
+    <li @click="parent()"><a href="#">../</a></li>
+    <li v-for="entry in filtered" :key="entry.name">
+      <div v-if="entry.type === 'directory'">
+        <a href="#" @click="move(entry.name)">{{ entry.name }}</a>
+        <span>/</span>
+      </div>
+      <div v-if="entry.type === 'file'">
+        <a :href="entrypoint + path + '/' + entry.name">
+          {{ entry.name }}
+        </a>
+      </div>
+    </li>
+  </ul>
 </template>
 
 <script lang="ts">
 import { Options, Vue } from "vue-class-component";
-import HelloWorld from "./components/HelloWorld.vue";
 
-@Options({
-  components: {
-    HelloWorld,
-  },
-})
-export default class App extends Vue {}
+interface FsEntry {
+  type: "directory" | "file";
+  name: string;
+  size?: number;
+  mtime: string;
+}
+
+@Options({})
+export default class App extends Vue {
+  entrypoint = "";
+
+  paths: string[] = [];
+  get path(): string {
+    return this.paths.join("/");
+  }
+
+  entries: FsEntry[] = [];
+
+  fetchItems(): void {
+    const URL = this.entrypoint + this.path;
+    fetch(URL)
+      .then((response: Response) => response.json())
+      .then((entries: FsEntry[]) => (this.entries = entries))
+      .catch((e) => console.log(e));
+  }
+
+  mounted(): void {
+    this.fetchItems();
+  }
+
+  move(str: string): void {
+    this.paths.push(str);
+    this.fetchItems();
+  }
+
+  parent(): void {
+    this.paths.pop();
+    this.fetchItems();
+  }
+  get filtered(): FsEntry[] {
+    return this.entries;
+  }
+}
 </script>
 
 <style>
